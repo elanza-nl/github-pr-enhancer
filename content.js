b4fe5ee319c776f52f9ac227e29d76bc92834583
CrazyStevenz <github.ekta@aleeas.com>
2026-02-05 14:09:56 +0200

    Abstract duplicated team icon

--- content.js
+++ content.js
@@ -23,6 +23,9 @@
   let allReviewers = new Map();
   let activeFilter = null;
   let filterBar = null;
+  const TEAM_ICON = `<svg aria-hidden="true" height="16" viewBox="0 0 16 16" width="16" class="octicon octicon-people">
+    <path d="M2 5.5a3.5 3.5 0 1 1 5.898 2.549 5.508 5.508 0 0 1 3.034 4.084.75.75 0 1 1-1.482.235 4 4 0 0 0-7.9 0 .75.75 0 0 1-1.482-.236A5.507 5.507 0 0 1 3.102 8.05 3.493 3.493 0 0 1 2 5.5ZM11 4a3.001 3.001 0 0 1 2.22 5.018 5.01 5.01 0 0 1 2.56 3.012.749.749 0 0 1-.885.954.752.752 0 0 1-.549-.514 3.507 3.507 0 0 0-2.522-2.372.75.75 0 0 1-.574-.73v-.352a.75.75 0 0 1 .416-.672A1.5 1.5 0 0 0 11 5.5.75.75 0 0 1 11 4Zm-5.5-.5a2 2 0 1 0-.001 3.999A2 2 0 0 0 5.5 3.5Z"></path>
+  </svg>`;
   let initializationTimeout = null;
 
   // Get GitHub API headers (includes token if available)
@@ -113,12 +116,8 @@
 
     const avatarElements = visibleReviewers.map((reviewer) => {
       if (reviewer.isTeam) {
-        // Team icon
-        const teamIcon = `<svg aria-hidden="true" height="16" viewBox="0 0 16 16" width="16" class="octicon octicon-people">
-          <path d="M2 5.5a3.5 3.5 0 1 1 5.898 2.549 5.508 5.508 0 0 1 3.034 4.084.75.75 0 1 1-1.482.235 4 4 0 0 0-7.9 0 .75.75 0 0 1-1.482-.236A5.507 5.507 0 0 1 3.102 8.05 3.493 3.493 0 0 1 2 5.5ZM11 4a3.001 3.001 0 0 1 2.22 5.018 5.01 5.01 0 0 1 2.56 3.012.749.749 0 0 1-.885.954.752.752 0 0 1-.549-.514 3.507 3.507 0 0 0-2.522-2.372.75.75 0 0 1-.574-.73v-.352a.75.75 0 0 1 .416-.672A1.5 1.5 0 0 0 11 5.5.75.75 0 0 1 11 4Zm-5.5-.5a2 2 0 1 0-.001 3.999A2 2 0 0 0 5.5 3.5Z"></path>
-        </svg>`;
         return `<span class="reviewer-team-badge tooltipped tooltipped-s" aria-label="@${reviewer.login}" data-login="${reviewer.login}" data-is-team="true">
-          ${teamIcon} <span class="team-name">@${reviewer.login}</span>
+          ${TEAM_ICON} <span class="team-name">@${reviewer.login}</span>
         </span>`;
       } else {
         // User avatar
@@ -378,15 +377,13 @@
       if (!hasPendingReviews(reviewer)) {
         continue;
       }
-      
+
       hasVisibleReviewers = true;
       const isActive = activeFilter && activeFilter.login === reviewer.login && activeFilter.isTeam === reviewer.isTeam;
-      
+
       if (reviewer.isTeam) {
         htmlContent += `<button class="reviewer-filter-team tooltipped tooltipped-s${isActive ? ' reviewer-filter-team--active' : ''}" aria-label="@${reviewer.login}">
-          <svg aria-hidden="true" height="16" viewBox="0 0 16 16" width="16" class="octicon octicon-people">
-            <path d="M2 5.5a3.5 3.5 0 1 1 5.898 2.549 5.508 5.508 0 0 1 3.034 4.084.75.75 0 1 1-1.482.235 4 4 0 0 0-7.9 0 .75.75 0 0 1-1.482-.236A5.507 5.507 0 0 1 3.102 8.05 3.493 3.493 0 0 1 2 5.5ZM11 4a3.001 3.001 0 0 1 2.22 5.018 5.01 5.01 0 0 1 2.56 3.012.749.749 0 0 1-.885.954.752.752 0 0 1-.549-.514 3.507 3.507 0 0 0-2.522-2.372.75.75 0 0 1-.574-.73v-.352a.75.75 0 0 1 .416-.672A1.5 1.5 0 0 0 11 5.5.75.75 0 0 1 11 4Zm-5.5-.5a2 2 0 1 0-.001 3.999A2 2 0 0 0 5.5 3.5Z"></path>
-          </svg>
+          ${TEAM_ICON}
         </button>`;
       } else {
         const avatarUrl = reviewer.avatarUrl || `https://github.com/${reviewer.login}.png?size=40`;
@@ -570,7 +567,7 @@
       element.addEventListener('mouseleave', hideTooltip);
       
       // Setup click handlers
-      element.addEventListener('click', (e) => {
+      element.addEventListener('click', () => {
         const isTeam = ariaLabel.startsWith('@');
         const login = isTeam ? ariaLabel.substring(1) : ariaLabel;
         toggleFilter(login, isTeam);
